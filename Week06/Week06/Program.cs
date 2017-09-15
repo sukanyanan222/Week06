2911c8356ea13f04b1fd8789d818a807da161fe4
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 10:00:55 +0700

    W06P06

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int b = add(1, 2, 3, 4, 5);
+        Console.WriteLine("b = {0}", b);
+        Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
+        Console.ReadLine();
+    }
+
+    static int add(params int[] i)
+    {
+        int sum = 0; ;
+        foreach (int j in i)
+            sum += j;
+        return sum;
     }
 }
