1440f4dc509aaf5c46970d78883a0450f1e51ca7
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 10:15:34 +0700

    W06P07

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        object[] myObjArray = { 2, 'b', 1000.05d, "Hello World" };
+        useParams(myObjArray);
+        useParams("IE.Tech.", "KMITL", 2017);
+        Console.ReadLine();
+    }
+
+    static void useParams(params object[] list)
+    {
+        foreach (object o in list)
+            Console.Write(o + " ");
+        Console.WriteLine();
     }
 }
