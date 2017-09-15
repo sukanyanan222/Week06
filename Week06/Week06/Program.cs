88b7b39bbbad7b1cfc24409f1e4b0673c148fe7e
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 10:46:52 +0700

    W06P10

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int val; // uninitialized variable
+        refMethod(out val);
+        Console.WriteLine(val);
+        Console.ReadLine();
+    }
+
+    static void refMethod(ref int i)
+    {
+        i = i + 1;
     }
 }
