f3d3a20508b4b8bccb5dfc1eb3976bdff810d412
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 09:48:48 +0700

    W06P05

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        long c = Factorial(5);
+        Console.WriteLine("Factorial of 5 = {0}", c);
+        Console.ReadLine();
+    }
+
+    static long Factorial(int n)
+    {
+        if (n == 0)  // Factorial 0 = 1
+            return 1;
+        return n * Factorial(n - 1);
     }
 }
