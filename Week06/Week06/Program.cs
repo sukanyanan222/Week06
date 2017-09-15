74fd654d97d01ef0ac987978a38bb1c22a729f96
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 09:45:41 +0700

    W06P04

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int a = 1;
+        int b = 2;
+        int c;
+        c = add(a, b);
+        Console.WriteLine("c = {0}", c);
+        Console.WriteLine("3 + 5 = {0}", add(3, 5));
+        Console.WriteLine("4 + 7 =  " + add(4, 7));
+        Console.ReadLine();
+    }
+
+    static int add(int a, int b)
+    {
+        return a + b;
     }
 }
