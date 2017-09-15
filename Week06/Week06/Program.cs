5fa40cf556a290eb52d4bce1d81895a625f544a1
Sukanya <Sukanyak21130@gmail.com>
2017-09-15 09:33:05 +0700

    W06P01

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
 
-namespace Week06
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        // Create web client.
+        WebClient client = new WebClient();
+        // Download string.
+        string value = client.DownloadString("http://www.kmitl.ac.th");
+        // Write values.
+        Console.WriteLine("--- WebClient result ---");
+        Console.WriteLine(value.Length);
+        Console.WriteLine(value);
+        StreamWriter outtxt = new StreamWriter("webload.html");
+        outtxt.Write(value);
+        outtxt.Dispose();
     }
 }
