bbc30bdfdff03eefad7d0620e658a06590c16d62
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 13:14:33 +0700

    W06P21

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,22 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+
+            long myLong = long.MaxValue;
+            int myInt = (int)myLong;   // Explicit conversion
+
+            float heightInMeters = 1.74f; // Explicit conversion
+            double maxHeight = heightInMeters; // Implicit
+            double minHeight = (double)heightInMeters; // Explicit
+            float actualHeight = (float)maxHeight; // Explicit
+
+            Console.WriteLine(myLong);
+            Console.WriteLine(myInt);
+            Console.WriteLine(heightInMeters);
+            Console.WriteLine(maxHeight);
+            Console.WriteLine(minHeight);
+            Console.WriteLine(actualHeight);
+
         }
     }
 }
