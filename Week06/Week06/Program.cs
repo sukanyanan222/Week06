e7f4d3e566a9fb020251aa1445db33af0494a8da
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 18:44:35 +0700

    W06P28

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -8,8 +8,19 @@ namespace Week06
 {
     class Program
     {
+        static void Moon(double S)
+        {
+            double Z;
+            Z = (S * 1.6);
+            Console.WriteLine("Weight on the moon is : {0} N.", Z);
+        }
         static void Main(string[] args)
         {
+            double S;
+            Console.Write("Enter the Mass : ");
+            S = double.Parse(Console.ReadLine());
+            Moon(S);
+            Console.ReadLine();
         }
     }
 }
