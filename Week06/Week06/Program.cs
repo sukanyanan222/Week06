55064daa121908283257b4dc8442df1e04ff7021
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 13:31:39 +0700

    W06P25

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,11 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+            int a = 2;
+            float f = 3.5f;
+            double d = 4.99d;
+            decimal money = 1254841354;
+            Console.WriteLine(a + " " + f + " " + d + " " + money);
         }
     }
 }
