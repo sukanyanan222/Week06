b9cfa1da41469b9d81692a4fc28acf466ab399e0
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 12:25:15 +0700

    W06P12

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,18 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+
+            int x = 6;
+            int y = 4;
+
+            Console.WriteLine(y *= 2);
+            int z = y = 3;
+
+            Console.WriteLine(z);
+            Console.WriteLine(x |= 1);
+            Console.WriteLine(x += 3);
+            Console.WriteLine(x /= 2);
+
         }
     }
 }
