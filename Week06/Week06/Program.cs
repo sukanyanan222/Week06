f10a3f3e451aaee164b1f1e739d5e802f392df54
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 13:06:43 +0700

    W06P18

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,14 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
+
         }
     }
 }
