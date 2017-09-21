84496fa051030a5a08c7746a37e8be65d5de458c
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 12:40:10 +0700

    W06P15

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,18 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+            Console.WriteLine(!b);
+            Console.WriteLine(b || true);
+            Console.WriteLine((9 > 7) ^ (a == b));
+            Console.WriteLine((b != a) || (a == b));
+            Console.WriteLine((b != a) && (a == b));
+            Console.WriteLine((!a) && (!b));
+            Console.WriteLine((!a) || (!b));
+
         }
     }
 }
