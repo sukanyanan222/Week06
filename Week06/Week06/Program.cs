ec01c7796961c27319ee3117429865a863fcc591
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 18:25:04 +0700

    W06P26

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -8,8 +8,27 @@ namespace Week06
 {
     class Program
     {
+        static void EvenOddNumber(double A)
+        {
+            string x;
+            if ((A % 2) > 0)
+            {
+                x = ("This is Odd Number");
+            }
+            else
+            {
+                x = ("This is Even Number");
+            }
+            Console.WriteLine("{0}", x);
+            return;
+        }
         static void Main(string[] args)
         {
-        }
+            double A;
+            Console.Write("Enter a number : ");
+            A = double.Parse(Console.ReadLine());
+            EvenOddNumber(A);
+            Console.ReadLine();
+        }   
     }
 }
