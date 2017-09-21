c6c2a22d95f810a1e5b030a3fee24c59b34fb0c7
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 12:36:58 +0700

    W06P14

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,15 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
+
         }
     }
 }
