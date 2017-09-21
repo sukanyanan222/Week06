b4452e75d1c33cb351c6e5714b2d841e9554cd8d
Sukanya <Sukanyak21130@gmail.com>
2017-09-21 13:12:50 +0700

    W06P20

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,6 +10,18 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+            double myDouble = 9.9d;
+            Console.WriteLine(myDouble);
+
+            long myLong = (long)myDouble;
+            Console.WriteLine(myLong);
+
+            myDouble = 9e9d;
+            Console.WriteLine(myDouble);
+
+            int myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+            Console.WriteLine(int.MinValue);
         }
     }
 }
